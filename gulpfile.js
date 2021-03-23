5b6e0f1bbab164617553553912e604c81888ee98
Tedir Ghazali <tedirghazali@gmail.com>
2021-03-23 09:14:20 +0700

    edit grid layout

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,6 @@ const terser = require('gulp-terser')
 const postcss = require('gulp-postcss')
 const path = require('path')
 
-const dist = {
-  css: './dist/',
-  js: './dist/',
-}
-
 const header = `@charset "UTF-8";
 
 /*!
@@ -36,47 +31,20 @@ function css() {
       require('postcss-header')({ header })
     ]))
     .pipe(rename("alga.css"))
-    .pipe(dest(dist.css, { sourcemaps: '.' }))
+    .pipe(dest('./dist/', { sourcemaps: '.' }))
 }
 
 function cssMin() {
-  return src(`${dist.css}alga.css`)
+  return src('dist/alga.css')
     .pipe(cleanCSS({ level: { 1: { specialComments: 0 } } }))
     .pipe(rename({ suffix: '.min' }))
-    .pipe(dest(dist.css))
-}
-
-function cssSlim() {
-  return src('css/slim.css', { sourcemaps: true })
-    .pipe(postcss([
-      require('postcss-import'),
-      require('postcss-mixins')({
-        mixinsDir: path.join(__dirname, 'css/mixins')
-      }),
-      require('tailwindcss'),
-      //require("stylelint"),
-      require('postcss-custom-properties'),
-      //require('postcss-simple-vars'),
-      require('postcss-nested'),
-      require('autoprefixer'),
-      require('postcss-header')({ header })
-    ]))
-    .pipe(rename("alga-slim.css"))
-    .pipe(dest(dist.css, { sourcemaps: '.' }))
-}
-
-function cssSlimMin() {
-  return src(`${dist.css}alga-slim.css`)
-    .pipe(cleanCSS({ level: { 1: { specialComments: 0 } } }))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(dest(dist.css))
+    .pipe(dest('./dist/'))
 }
 
 function watchChanges() {
   watch('css/app.css', series(css, cssMin))
-  watch('css/slim.css', series(cssSlim, cssSlimMin))
 }
 
 exports.watch = watchChanges
-exports.build = series(css, cssMin, cssSlim, cssSlimMin)
-exports.default = series(css, cssMin, cssSlim, cssSlimMin) //watchChanges
+exports.build = series(css, cssMin)
+exports.default = series(css, cssMin) //watchChanges
