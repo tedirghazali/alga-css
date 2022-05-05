49a6a54ddb9262afccd45d960d0e90c73255c263
Tedir Ghazali <tedirghazali@gmail.com>
2022-05-05 10:54:48 +0700

    List Group

---
+++ test/components/list.test.js
@@ -0,0 +1,34 @@
+const execute = require('../execute.js')
+
+test('Testing list group component', async () => {
+  const input = '@use list;'
+  const output = `.list {
+    width: 100%;
+    background-color: #fff;
+    border: 1px solid #d9d9d9;
+    border-radius: 0.375rem
+}
+.list .listWrap {
+    padding: 0.5rem;
+    border-bottom: 1px solid #d9d9d9
+}
+.list .listGroup {
+    overflow-y: auto;
+    max-height: calc(10 * 36px)
+}
+.list .listItem {
+    display: block;
+    padding: 0.5rem 0.75rem;
+    border-bottom: 1px solid #d9d9d9;
+    text-decoration: none;
+    color: #283541
+}
+.list .listItem:last-child {
+    border-bottom: 0
+}
+.list.sizing {
+    overflow-y: auto;
+    max-height: calc(10 * 36px)
+}`
+  await execute(input, output, {log: false, file: './examples/list/list.css'})
+})
