554be5dcb47a823816dce1ae4830090dd252e0f3
Tedir Ghazali <tedirghazali@gmail.com>
2022-07-24 10:46:04 +0700

    set map inline false

--- src/cores/package.js
+++ src/cores/package.js
@@ -17,7 +17,7 @@ function readPath(rp, opts) {
   component[componentName]['modules'] = {}
   component[componentName]['inits'] = []
   
-  const root = postcss.parse(data, { from: undefined })
+  const root = postcss.parse(data, { from: undefined, map: { inline: false } })
   for(let rnode of root.nodes) {
     // Convert define into property
     if(rnode.type === 'atrule' && rnode.name === 'import') {
